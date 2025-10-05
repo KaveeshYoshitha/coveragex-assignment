639bf88bb7b8d479d0dbc7c9f99e2d4ec0ff20b1
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2025-10-05 20:57:48 +0530

    Dockerize fullstack todo app
    Dockerize project with MySQL, Node.js backend, and React frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const dbName = process.env.NODE_ENV === "test" ? process.env.DB_NAME_TEST : process.env.DB_NAME;
+// const dbName = process.env.NODE_ENV === "test" ? process.env.DB_NAME_TEST : process.env.DB_NAME;
 
 
 const pool  = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: dbName,
+  // database: dbName,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
